4ed7cba625ec22c42500c85e4defb4d47135fa59
Flying <iamflying@vip.qq.com>
2021-06-10 11:44:13 +0800

    feat(objc): 添加 debug target

---
+++ configured/objc4-824/lpf-objc-debug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lpf-objc-debug
+//
+//  Created by Flying on 2021/6/10.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
